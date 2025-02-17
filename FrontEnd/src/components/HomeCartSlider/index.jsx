56bed53149956005e13cd7beae3afb6218b67249
Tri Tan <vuutritan2k2@gmail.com>
2025-02-17 10:59:55 +0700

    Cap Nhat

--- FrontEnd/src/components/HomeCartSlider/index.jsx
+++ FrontEnd/src/components/HomeCartSlider/index.jsx
@@ -32,6 +32,71 @@ const HomeCartSlider = () => {
               </div>
             </Link>
           </SwiperSlide>
+
+          <SwiperSlide>
+            <Link to={`/product/1`}>
+              <div className="item py-7 px-3 bg-white rounded-sm flex text-center items-center justify-center flex-col">
+                <img
+                  src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/h_451,c_limit/981757c4-1988-4410-a0fb-fecd783fe41d/air-jordan-1-mid-shoes-SQf7DM.png"
+                  alt="Air Jordan 1 Mid"
+                  className="transition-all duration-300 ease-in-out transform hover:scale-105"
+                />
+                <h3 className="text-[16px] font-[500] mt-2">Air Jordan 1 Mid</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Link to={`/product/1`}>
+              <div className="item py-7 px-3 bg-white rounded-sm flex text-center items-center justify-center flex-col">
+                <img
+                  src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/h_451,c_limit/981757c4-1988-4410-a0fb-fecd783fe41d/air-jordan-1-mid-shoes-SQf7DM.png"
+                  alt="Air Jordan 1 Mid"
+                  className="transition-all duration-300 ease-in-out transform hover:scale-105"
+                />
+                <h3 className="text-[16px] font-[500] mt-2">Air Jordan 1 Mid</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Link to={`/product/1`}>
+              <div className="item py-7 px-3 bg-white rounded-sm flex text-center items-center justify-center flex-col">
+                <img
+                  src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/h_451,c_limit/981757c4-1988-4410-a0fb-fecd783fe41d/air-jordan-1-mid-shoes-SQf7DM.png"
+                  alt="Air Jordan 1 Mid"
+                  className="transition-all duration-300 ease-in-out transform hover:scale-105"
+                />
+                <h3 className="text-[16px] font-[500] mt-2">Air Jordan 1 Mid</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Link to={`/product/1`}>
+              <div className="item py-7 px-3 bg-white rounded-sm flex text-center items-center justify-center flex-col">
+                <img
+                  src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/h_451,c_limit/981757c4-1988-4410-a0fb-fecd783fe41d/air-jordan-1-mid-shoes-SQf7DM.png"
+                  alt="Air Jordan 1 Mid"
+                  className="transition-all duration-300 ease-in-out transform hover:scale-105"
+                />
+                <h3 className="text-[16px] font-[500] mt-2">Air Jordan 1 Mid</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <Link to={`/product/1`}>
+              <div className="item py-7 px-3 bg-white rounded-sm flex text-center items-center justify-center flex-col">
+                <img
+                  src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/h_451,c_limit/981757c4-1988-4410-a0fb-fecd783fe41d/air-jordan-1-mid-shoes-SQf7DM.png"
+                  alt="Air Jordan 1 Mid"
+                  className="transition-all duration-300 ease-in-out transform hover:scale-105"
+                />
+                <h3 className="text-[16px] font-[500] mt-2">Air Jordan 1 Mid</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
