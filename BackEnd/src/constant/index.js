4172e325e1cccb87d90ac296fa24e1ac3ef901d4
vuutritan2k2 <vuutritan2k2@gmail.com>
2025-03-13 17:58:55 +0700

    Delete BackEnd directory

--- BackEnd/src/constant/index.js
+++
@@ -1,3 +0,0 @@
-import { ROLE } from './role.js';
-
-export { ROLE };
--- BackEnd/src/routes/uploadRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { uploadImg } from '../controllers/uploadController.js';
-
-const uploadRouter = express.Router();
-
-uploadRouter.post('/', uploadImg);
-
-export default uploadRouter;
