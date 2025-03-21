31aed7f4323a1f4bf1c20c981e162bc683187459
TuanNghia295 <tuannghiait2905@gmail.com>
2025-03-21 17:24:59 +0700

    18 - APIs lấy thông tin, cập nhật người dùng hiện tại. Xóa và cập nhật người dùng theo ID

--- BackEnd/src/controllers/userController.js
+++ BackEnd/src/controllers/userController.js
@@ -9,7 +9,15 @@ export const getListUser = async (req, res) => {
   const pageNumber = parseInt(page);
 
   // tạo truy vấn q trên MongoDB với option i dùng để không phân biệt hoa thường
-  const query = q ? { name: { $regex: q, $options: 'i' } } : {};
+  const query = q
+    ? {
+        $or: [
+          { fullName: { $regex: q, $options: 'i' } },
+          { userName: { $regex: q, $options: 'i' } },
+          { phone: { $regex: q, $options: 'i' } },
+        ],
+      }
+    : {};
   const sortBy = order === 'ASC' ? 1 : -1;
 
   // Đếm tổng số người dùng thõa query
@@ -33,4 +41,85 @@ export const getListUser = async (req, res) => {
 export const getUserInfo = async (req, res) => {
   const user = await UserModel.findById(req.user._id).select('-password'); // Lấy thông tin người dùng hiện tại, không lấy password
   res.json(user);
+};
+
+// Sửa thông tin người dùng hiện tại
+export const updateUser = async (req, res) => {
+  const { userName, fullName, password, email, phone, address } = req.body;
+
+  try {
+    // Tìm người dùng trong cơ sở dữ liệu
+    const user = await UserModel.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Cập nhật thông tin người dùng
+    if (userName) user.userName = userName;
+    if (fullName) user.fullName = fullName;
+    if (password) user.password = await bcrypt.hash(password, 10); // Mã hóa mật khẩu mới
+    if (email) user.email = email;
+    if (phone) user.phone = phone;
+    if (address) user.address = address;
+
+    // Lưu thông tin người dùng đã cập nhật
+    await user.save();
+
+    // Trả về thông tin người dùng đã cập nhật, không bao gồm mật khẩu
+    const updatedUser = await UserModel.findById(req.user._id).select('-password');
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating user', error: error.message });
+  }
+};
+
+// Xóa người dùng
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    // Tìm người dùng trong cơ sở dữ liệu
+    const user = await UserModel.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    // Xóa người dùng
+    await user.remove();
+    res.json({ statusCode: 200, message: 'User deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting user', error: error.message });
+  }
+};
+
+// Cập nhật người dùng theo id
+export const updateUserById = async (req, res) => {
+  const { id } = req.params;
+  const { userName, fullName, password, email, phone, role, address } = req.body;
+
+  try {
+    // Tìm người dùng trong cơ sở dữ liệu
+    const user = await UserModel.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Cập nhật thông tin người dùng
+    if (userName) user.userName = userName;
+    if (fullName) user.fullName = fullName;
+    if (password) user.password = await bcrypt.hash(password, 10); // Mã hóa mật khẩu mới
+    if (email) user.email = email;
+    if (phone) user.phone = phone;
+    if (role) user.role = role;
+    if (address) user.address = address;
+
+    // Lưu thông tin người dùng đã cập nhật
+    await user.save();
+
+    // Trả về thông tin người dùng đã cập nhật, không bao gồm mật khẩu
+    const updatedUser = await UserModel.findById(id).select('-password');
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating user', error: error.message });
+  }
 };
--- BackEnd/src/routes/userRouter.js
+++ BackEnd/src/routes/userRouter.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
-import { getListUser, getUserInfo } from '../controllers/userController.js';
+import { deleteUser, getListUser, getUserInfo, updateUser, updateUserById } from '../controllers/userController.js';
 import authAdminMiddleware from '../middleware/authAdminMiddleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/getAll', authAdminMiddleware, getListUser);
 userRouter.get('/userInfo', authMiddleware, getUserInfo);
+userRouter.patch('/updateUser', authMiddleware, updateUser);
+userRouter.patch('/updateUser/:id', authAdminMiddleware, updateUserById);
+userRouter.get('/delete/:id', authMiddleware, deleteUser);
 
 export default userRouter;
