5ab7f3a08ac5bbd6269ddc82d95b9ca14198af59
Tri Tan <vuutritan2k2@gmail.com>
2025-04-01 20:50:58 +0700

    17.4 - Delete Modal VNPAY trong Check Out Page

--- FrontEnd/src/components/HomeSlider/index.jsx
+++ FrontEnd/src/components/HomeSlider/index.jsx
@@ -14,7 +14,7 @@ const HomeSlider = () => {
       }}
       loop={true}
       autoplay={{
-        delay: 3000,
+        delay: 30000,
         reverseDirection: false,
       }}
       modules={[Autoplay, Pagination]}
@@ -40,7 +40,15 @@ const HomeSlider = () => {
     >
       <SwiperSlide>
         <Link to={'/listing'}>
-          <img src={jordanImage} alt="Jordan" />
+          <div className='w-full relative'>
+            <img className='object-center w-full' src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1824,c_limit/bba76ce9-61bf-466b-b0f5-ac779feb333c/nike-just-do-it.png" alt="Jordan" />
+            <div className='w-full absolute bottom-[30%] flex flex-col items-center justify-center text-white z-20'>
+              <p className='px-4 py-2 bg-red-500 text-white rounded-full'>Comming Soon !</p>
+              <h1 className='text-white text-[50px] xl:text-[200px] font-[800] uppercase'>Juka 4</h1>
+              <p className='text-center hidden xl:block w-1/2'>Luka Dončić plays with zero remorse. He hits game winners without breaking a sweat and leaves defenders looking clueless—and he never feels bad about it. So what sort of shoes do you design for someone like that? They've got to be comfortable with firm support—ideal for step-backs, euro steps and playing bully ball in the post. Flightwire cables. IsoPlate. Cushlon foam. Air Zoom unit. It's all got to be in there. In short, you've got to give a bad man some really nice shoes.</p>
+            </div>
+            <div className='bg-black w-full h-full absolute top-0 left-0 opacity-50'></div>
+          </div>
         </Link>
       </SwiperSlide>
       <SwiperSlide>
