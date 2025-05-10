5a2c016d629efdc41d8d553baa99f8d663142c30
TuanNghia295 <tuannghiait2905@gmail.com>
2025-05-10 12:06:20 +0700

    38.3 Cập nhât firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,32 +1,12 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import fs from 'fs';
+
 dotenv.config();
 
-const serviceAccountFilePath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_JSON;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-if (!serviceAccountFilePath) {
-  console.error('Firebase service account key file path not found in environment variables.');
-  process.exit(1);
-}
-
-let serviceAccount;
-try {
-  const serviceAccountContent = fs.readFileSync(serviceAccountFilePath, 'utf8');
-  serviceAccount = JSON.parse(serviceAccountContent);
-} catch (error) {
-  console.error('Failed to read or parse Firebase service account key file:', error);
-  process.exit(1);
-}
-
-try {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-  console.log('Firebase Admin SDK initialized successfully');
-} catch (error) {
-  console.error('Firebase Admin SDK initialization failed:', error);
-  process.exit(1);
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 export default admin;
